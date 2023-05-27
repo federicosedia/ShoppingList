3589ea2a57a124fcabfae39fbda2cb57d43e807f
Federico Sedia <federicosdia@gmail.com>
2023-05-27 15:44:59 +0200

    Aggiunta menù a tendina e TextFormField

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -39,6 +40,62 @@ class _NewItemState extends State<NewItem> {
                 },
                 maxLines: 3,
                 minLines: 1,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  //quantità. Non essendo vincolato orizzontalmente
+                  //devo vincolarlo io con expandend altrimenti ricevo un errore
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text(
+                          'Quantity',
+                        ),
+                      ),
+                      //valore iniziale "stringa"
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  //pulsante a discesa per scegliere una categoria
+                  //anche qui come sopra devo vincolarlo orizzontalmente con expandend
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      //lista di categorie ma le categorie essendo una mappa
+                      //verrà richiamato con entries per convertirlo
+                      //è una proprietà che fornisce un iterabile che contiene le mappe chiave valore
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            //nel menù a tendina vedrò tutti i valori associati
+                            //quindi colore e titolo
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                //container per box con colore della categoria
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(
+                                  width: 6,
+                                ),
+                                //testo da mostrare ovvero nome della categoria
+                                //qui abbiamo sia key che value perchè con entries abbiamo l'iterabile della mappa
+                                //quello che interessa a noi però è il valore della chiave e non la chiave
+                                Text(category.value.title),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  )
+                ],
               )
             ],
           ),
